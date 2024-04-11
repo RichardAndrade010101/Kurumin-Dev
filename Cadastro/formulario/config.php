e4bd81277f07fb2f65f21b57f66f837287dd9ee0
RichardAndrade010101 <andradericharde@gmail.com>
2024-04-11 16:12:35 -0300

    att

--- Cadastro/formulario/config.php
+++ Cadastro/formulario/config.php
@@ -1,19 +1,17 @@
 <?php
 
-    $dbHost = 'Localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'cadastro_kurumin_dev';
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'cadastro_kurumin_dev';
 
-    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-    if ($conexao->connect_errno)
-    {
-        echo "Erro.";
-    }
-    else
-    {
-        echo "Conexãp efetuada com sucesso.";
-    }
-
-?>
+/// if ($conexao->connect_errno)
+//// {
+//     echo "Erro.";
+//}
+//else
+//{
+//     echo "Conexãp efetuada com sucesso.";
+//} 
--- Cadastro/formulario/formulario.php
+++ Cadastro/formulario/formulario.php
@@ -1,40 +1,53 @@
+<?php
+if (isset($_POST['submit'])) {
+    print_r($_POST['nome']);
+    print_r($_POST['email']);
+    print_r($_POST['telefone']);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário | GN</title>
     <style>
-        body{
+        body {
             font-family: Arial, Helvetica, sans-serif;
             background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
         }
-        .box{
+
+        .box {
             color: white;
             position: absolute;
             top: 50%;
             left: 50%;
-            transform: translate(-50%,-50%);
+            transform: translate(-50%, -50%);
             background-color: rgba(0, 0, 0, 0.6);
             padding: 15px;
             border-radius: 15px;
             width: 20%;
         }
-        fieldset{
+
+        fieldset {
             border: 3px solid dodgerblue;
         }
-        legend{
+
+        legend {
             border: 1px solid dodgerblue;
             padding: 10px;
             text-align: center;
             background-color: dodgerblue;
             border-radius: 8px;
         }
-        .inputBox{
+
+        .inputBox {
             position: relative;
         }
-        .inputUser{
+
+        .inputUser {
             background: none;
             border: none;
             border-bottom: 1px solid white;
@@ -44,28 +57,32 @@
             width: 100%;
             letter-spacing: 2px;
         }
-        .labelInput{
+
+        .labelInput {
             position: absolute;
             top: 0px;
             left: 0px;
             pointer-events: none;
             transition: .5s;
         }
-        .inputUser:focus ~ .labelInput,
-        .inputUser:valid ~ .labelInput{
+
+        .inputUser:focus~.labelInput,
+        .inputUser:valid~.labelInput {
             top: -20px;
             font-size: 12px;
             color: dodgerblue;
         }
-        #data_nascimento{
+
+        #data_nascimento {
             border: none;
             padding: 8px;
             border-radius: 10px;
             outline: none;
             font-size: 15px;
         }
-        #submit{
-            background-image: linear-gradient(to right,rgb(0, 92, 197), rgb(90, 20, 220));
+
+        #submit {
+            background-image: linear-gradient(to right, rgb(0, 92, 197), rgb(90, 20, 220));
             width: 100%;
             border: none;
             padding: 15px;
@@ -74,14 +91,16 @@
             cursor: pointer;
             border-radius: 10px;
         }
-        #submit:hover{
-            background-image: linear-gradient(to right,rgb(0, 80, 172), rgb(80, 19, 195));
+
+        #submit:hover {
+            background-image: linear-gradient(to right, rgb(0, 80, 172), rgb(80, 19, 195));
         }
     </style>
 </head>
+
 <body>
     <div class="box">
-        <form action="">
+        <form action="formulario.php" method="POST">
             <fieldset>
                 <legend><b>Fórmulário de Clientes</b></legend>
                 <br>
@@ -132,4 +151,5 @@
         </form>
     </div>
 </body>
+
 </html>
